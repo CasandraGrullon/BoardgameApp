027c4d57dd4bb7df689441927a47d647d0afc48c
Casandra <casandragrullon@pursuit.org>
2020-04-30 15:18:14 -0400

    finished api client functions

--- BoardgameApp/NetworkServices/BoardGameAPIClient.swift
+++ BoardgameApp/NetworkServices/BoardGameAPIClient.swift
@@ -22,8 +22,8 @@ struct BoardGameAPIClient {
         let request = URLRequest(url: url)
         NetworkHelper.shared.performDataTask(with: request) { (result) in
             switch result {
-            case .failure(let error):
-                completion(.failure(.networkClientError(error)))
+            case .failure(let getGamesError):
+                completion(.failure(.networkClientError(getGamesError)))
             case .success(let data):
                 do {
                     let results = try JSONDecoder().decode(GameResults.self, from: data)
@@ -37,5 +37,49 @@ struct BoardGameAPIClient {
         
     }
     
+    static func getReviews(gameId: String, completion: @escaping (Result<[Reviews], AppError>) -> ()) {
+        let endpointURL = "https://www.boardgameatlas.com/api/reviews?pretty=true&client_id=\(Secrets.clientId)&game_id=\(gameId)"
+        guard let url = URL(string: endpointURL) else {
+            completion(.failure(.badURL(endpointURL)))
+            return
+        }
+        let request = URLRequest(url: url)
+        NetworkHelper.shared.performDataTask(with: request) { (result) in
+            switch result {
+            case .failure(let getReviewsError):
+                completion(.failure(.networkClientError(getReviewsError)))
+            case .success(let data):
+                do {
+                    let results = try JSONDecoder().decode([Reviews].self, from: data)
+                    completion(.success(results))
+                } catch {
+                    completion(.failure(.decodingError(error)))
+                }
+            }
+        }
+    }
+    
+    static func getGameCategories(completion: @escaping (Result<[Category], AppError>) -> ()) {
+        let endpointURL = "https://www.boardgameatlas.com/api/game/categories?client_id=\(Secrets.clientId)"
+        guard let url = URL(string: endpointURL) else {
+            completion(.failure(.badURL(endpointURL)))
+            return
+        }
+        let request = URLRequest(url: url)
+        
+        NetworkHelper.shared.performDataTask(with: request) { (result) in
+            switch result {
+            case .failure(let getCategoriesError):
+                completion(.failure(.networkClientError(getCategoriesError)))
+            case .success(let data):
+                do {
+                    let results = try JSONDecoder().decode(Categories.self, from: data)
+                    completion(.success(results.categories))
+                } catch {
+                    completion(.failure(.decodingError(error)))
+                }
+            }
+        }
+    }
     
 }
