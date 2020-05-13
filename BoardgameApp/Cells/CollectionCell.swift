e7766c108d990edf6a201faa488c087bba5ff0e8
Casandra <casandragrullon@pursuit.org>
2020-05-13 11:13:12 -0400

    added collection cell

---
+++ BoardgameApp/Cells/CollectionCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionCell.swift
+//  BoardgameApp
+//
+//  Created by casandra grullon on 5/13/20.
+//  Copyright © 2020 casandra grullon. All rights reserved.
+//
+
+import UIKit
+
+class CollectionCell: UICollectionViewCell {
+    
+}
